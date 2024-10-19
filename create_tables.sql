c50482fddf80cfa0326c6e0ab4ac40b018c4fff3
singhtanveer38 <singhtanveer38@yahoo.com>
2024-10-19 05:52:31 +0530

    db setup

--- create_tables.sql
+++ create_tables.sql
@@ -6,9 +6,28 @@ create table marks(
 	exam_name varchar(16),
 	exam_marks int,
 	subject varchar(16),
-	marks_obtained int
+	marks_obtained float
 );
 
 create table overall_result(
+	roll_no int,
+	name varchar(128),
+	class int,
+	section char,
+	exam_name varchar(16),
+	exam_total int,
+	percentage float,
+	category varchar(16)
+);
 
+create table subject_result(
+	roll_no int,
+	name varchar(128),
+	class int,
+	section char,
+	exam_name varchar(16),
+	exam_marks int,
+	marks_obtained float,
+	percentage float,
+	category varchar(16)
 );
--- setup.py
+++ setup.py
@@ -2,7 +2,20 @@ import psycopg2
 import yaml
 
 def connect(hostname, port, user, password, database):
-    conn = psycopg2.connect(hostname=hostname, port=port, user=user, password=password, database=database)
+    
+    conn = psycopg2.connect(host=hostname, port=port, user=user, password=password, database="postgres")
+    conn.autocommit = True
+    cur = conn.cursor()
+    
+    try:
+        cur.execute(f"create database {database}")
+    except:
+        print("Database already exists, skipping database creation")
+
+    close_connection(conn)
+
+    conn = psycopg2.connect(host=hostname, port=port, user=user, password=password, database=database)
+    conn.autocommit = True
     cur = conn.cursor()
 
     return conn, cur
@@ -10,11 +23,15 @@ def connect(hostname, port, user, password, database):
 def close_connection(conn):
     conn.close()
 
-def create_tables(conn, cur):
-    cur.execute(query1)
-
+def create_tables(cur):
+    with open("./create_tables.sql", "r") as f:
+        sql = f.readlines()
+        sql = "".join(sql)
+    cur.execute(sql)
 
 with open("./config.yaml", "r") as f:
     cred = yaml.load(f, Loader=yaml.FullLoader)
 
-print(cred)
+conn, cur = connect(cred["hostname"], cred["port"], cred["username"], cred["password"], cred["db_name"])
+create_tables(cur)
+close_connection(conn)
