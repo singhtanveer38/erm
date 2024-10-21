876079eeff3df3c8f204af227ee0238e813a2796
singhtanveer38 <singhtanveer38@yahoo.com>
2024-10-21 07:04:00 +0530

    raw files processed

--- load.py
+++ load.py
@@ -1,4 +1,5 @@
 import yaml
+import numpy as np
 import pandas as pd
 import os
 
@@ -6,6 +7,29 @@ with open("./config.yaml", "r") as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 
 dataDir = config["data_dir"]
+processedDir = config["processed_dir"]
 
-for file in dataDir:
-    pass
+
+if not os.path.exists(processedDir):
+    os.makedirs(processedDir)
+
+
+for file in os.listdir(dataDir):
+    print(file)
+    if file.split(".")[-1] == "csv":
+        print("Processing "+file)
+        df = pd.read_csv(dataDir+file)
+        std, sec, exam, exam_total = file.split("_")[1:]
+        exam_total = exam_total.split(".")[0]
+
+        df["class"] = std
+        df["section"] = sec
+        df["exam"] = exam
+        df["exam_total"] = exam_total
+
+        df = df.melt(id_vars=["roll no", "name", "class", "section", "exam", "exam_total"], var_name="subject", value_name="marks")
+
+        df = df.replace(["AB", "Ab", "ab", "ABSENT", "ABSENT "], np.nan)
+
+        df.to_csv(processedDir+file, index=None)
+        print(file+" processing done")
