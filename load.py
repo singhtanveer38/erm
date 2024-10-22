1fd6d723a8cf0ceb2a67cefb3212312da58024f7
singhtanveer38 <singhtanveer38@yahoo.com>
2024-10-22 06:18:15 +0530

    load setup merged

--- load.py
+++ load.py
@@ -3,33 +3,36 @@ import numpy as np
 import pandas as pd
 import os
 
-with open("./config.yaml", "r") as f:
-    config = yaml.load(f, Loader=yaml.FullLoader)
+def preprocessing():
+    with open("./config.yaml", "r") as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
 
-dataDir = config["data_dir"]
-processedDir = config["processed_dir"]
-
-
-if not os.path.exists(processedDir):
-    os.makedirs(processedDir)
+    dataDir = config["data_dir"]
+    processedDir = config["processed_dir"]
 
 
-for file in os.listdir(dataDir):
-    print(file)
-    if file.split(".")[-1] == "csv":
-        print("Processing "+file)
-        df = pd.read_csv(dataDir+file)
-        std, sec, exam, exam_total = file.split("_")[1:]
-        exam_total = exam_total.split(".")[0]
+    if not os.path.exists(processedDir):
+        os.makedirs(processedDir)
 
-        df["class"] = std
-        df["section"] = sec
-        df["exam"] = exam
-        df["exam_total"] = exam_total
 
-        df = df.melt(id_vars=["roll no", "name", "class", "section", "exam", "exam_total"], var_name="subject", value_name="marks")
+    for file in os.listdir(dataDir):
+        print(file)
+        if file.split(".")[-1] == "csv":
+            print("Processing "+file)
+            df = pd.read_csv(dataDir+file)
+            std, sec, exam, exam_total = file.split("_")[1:]
+            exam_total = exam_total.split(".")[0]
 
-        df = df.replace(["AB", "Ab", "ab", "ABSENT", "ABSENT "], np.nan)
+            df["class"] = std
+            df["section"] = sec
+            df["exam"] = exam
+            df["exam_total"] = exam_total
 
-        df.to_csv(processedDir+file, index=None)
-        print(file+" processing done")
+            df = df.melt(id_vars=["roll no", "name", "class", "section", "exam", "exam_total"], var_name="subject", value_name="marks")
+
+            df = df.replace(["AB", "Ab", "ab", "ABSENT", "ABSENT "], np.nan)
+
+            df.to_csv(processedDir+file, index=None)
+            print(file+" processing done")
+
+def into_db(conn, curr):
--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 import psycopg2
 import yaml
+import numpy as np
+import pandas as pd
+import os
 
 def connect(hostname, port, user, password, database):
-
     conn = psycopg2.connect(host=hostname, port=port, user=user, password=password, database="postgres")
     conn.autocommit = True
     cur = conn.cursor()
@@ -25,12 +27,51 @@ def create_tables(cur):
         sql = "".join(sql)
     cur.execute(sql)
 
-with open("./config.yaml", "r") as f:
-    cred = yaml.load(f, Loader=yaml.FullLoader)
+def read_config():
+    with open("./config.yaml", "r") as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+    return config
 
-try:
-    conn, cur = connect(cred["hostname"], cred["port"], cred["username"], cred["password"], cred["db_name"])
-    create_tables(cur)
-    close_connection(conn)
-except:
-    print("Database or table already exist")
+def preprocessing():
+    config = read_config()
+
+    dataDir = config["data_dir"]
+    processedDir = config["processed_dir"]
+
+    if not os.path.exists(processedDir):
+        os.makedirs(processedDir)
+
+    for file in os.listdir(dataDir):
+        print(file)
+        if file.split(".")[-1] == "csv":
+            print("Processing "+file)
+            df = pd.read_csv(dataDir+file)
+            std, sec, exam, exam_total = file.split("_")[1:]
+            exam_total = exam_total.split(".")[0]
+
+            df["class"] = std
+            df["section"] = sec
+            df["exam"] = exam
+            df["exam_total"] = exam_total
+
+            df = df.melt(id_vars=["roll no", "name", "class", "section", "exam", "exam_total"], var_name="subject", value_name="marks")
+
+            df = df.replace(["AB", "Ab", "ab", "ABSENT", "ABSENT "], np.nan)
+
+            df.to_csv(processedDir+file, index=None)
+            print(file+" processing done")
+
+def into_db(curr, processed_dir):
+    for file in os.listdir(processed_dir):
+
+
+
+# with open("./config.yaml", "r") as f:
+#     cred = yaml.load(f, Loader=yaml.FullLoader)
+
+# try:
+#     conn, cur = connect(cred["hostname"], cred["port"], cred["username"], cred["password"], cred["db_name"])
+#     create_tables(cur)
+#     close_connection(conn)
+# except:
+#     print("Database or table already exist")
