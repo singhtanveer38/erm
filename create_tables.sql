cce79f1b895352537848d4faf84e4b55ad293bd2
singhtanveer38 <singhtanveer38@yahoo.com>
2024-10-27 00:45:51 +0530

    tables changed

--- create_tables.sql
+++ create_tables.sql
@@ -6,31 +6,34 @@ create table marks(
 	exam_name varchar(16),
 	exam_marks int,
 	subject varchar(16),
-	marks_obtained float
-);
-
-create table overall_result(
-	roll_no int,
-	name varchar(128),
-	class int,
-	section char,
-	exam_name varchar(16),
-	exam_total int,
-	percentage float,
-	category varchar(16)
-);
-
-create table subject_result(
-	roll_no int,
-	name varchar(128),
-	class int,
-	section char,
-	exam_name varchar(16),
-	exam_marks int,
 	marks_obtained float,
+	attendence varchar(8),
 	percentage float,
 	category varchar(16)
 );
+
+-- create table overall_result(
+-- 	roll_no int,
+-- 	name varchar(128),
+-- 	class int,
+-- 	section char,
+-- 	exam_name varchar(16),
+-- 	exam_total int,
+-- 	percentage float,
+-- 	category varchar(16)
+-- );
+
+-- create table subject_result(
+-- 	roll_no int,
+-- 	name varchar(128),
+-- 	class int,
+-- 	section char,
+-- 	exam_name varchar(16),
+-- 	exam_marks int,
+-- 	marks_obtained float,
+-- 	percentage float,
+-- 	category varchar(16)
+-- );
 
 create table loaded_files(
 	timestamp timestamp,
--- helper.py
+++ helper.py
@@ -47,14 +47,49 @@ def preprocessing(dataDir, processedDir):
             std, sec, exam, exam_total = file.split("_")[1:]
             exam_total = exam_total.split(".")[0]
 
-            df["class"] = std
+            df["class"] = int(std)
             df["section"] = sec
             df["exam"] = exam
-            df["exam_total"] = exam_total
+            df["exam_total"] = int(exam_total)
 
             df = df.melt(id_vars=["roll no", "name", "class", "section", "exam", "exam_total"], var_name="subject", value_name="marks")
 
-            df = df.replace(["AB", "Ab", "ab", "ABSENT", "ABSENT ", np.nan], 0)
+            df = df.fillna(0)
+
+            attendence = []
+            marks_new = []
+            for i in df["marks"]:
+                if i in ["AB", "Ab", "ab", "ABSENT", "ABSENT "]:
+                    attendence.append("absent")
+                    marks_new.append(0)
+                else:
+                    attendence.append("present")
+                    marks_new.append(float(i))
+
+            df["attendence"] = attendence
+            df["marks"] = marks_new
+            print(type(df["marks"][0]))
+            df["percentage"] = round((df["marks"]/df["exam_total"]) * 100, 2)
+
+            category = []
+            for attendence, percentage in zip(df["attendence"], df["percentage"]):
+                if attendence == "absent":
+                    category.append("absent")
+                else:
+                    if percentage < 33:
+                        category.append("0_to_33")
+                    elif 33 <= percentage < 45:
+                        category.append("33_to_45")
+                    elif 45 <= percentage < 60:
+                        category.append("45_to_60")
+                    elif 60 <= percentage < 75:
+                        category.append("60_to_75")
+                    elif 75 <= percentage < 90:
+                        category.append("75_to_90")
+                    else:
+                        category.append("90_to_100")
+
+            df["category"] = category
 
             df.to_csv(processedDir+file, index=None, header=None)
             print(file+" processing done")
